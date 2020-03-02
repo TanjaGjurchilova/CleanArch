70636491ee678d6b10ae9e3346a6dca46592f560
Chudna <gavrovska.tanja@gmail.com>
2020-03-02 12:52:04 +0100

    Commit Service and Repository Interfaces

---
+++ CleanArch.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Interfaces
+{
+    interface IUserRepository
+    {
+        IEnumerable<User> Users();
+        User GetUser(int UserId);
+        bool InsertUser(User User);
+        User DeleteUser(int UserId);
+        bool UpdateUser(User User);
+
+        User ValidateUser(string username, string password);
+        bool ChangeUserPassword(int userId, string oldPassword, string newPassword);
+        string CreateResetPasswordToken(string usermail, string path);
+        bool ResetUserPassword(int userId, out string pass);
+        User LoginUserWithoutHash(string email, string password);
+    }
+}
